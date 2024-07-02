68e29d37b4b001bb916b89141ed77842444a984e
kirtiiguptaa <kirtigupta9449@gmail.com>
2024-07-02 23:50:04 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const menuIcon = document.querySelector(".menu-icon");
+    const navLists = document.querySelector(".nav-lists");
+
+    menuIcon.addEventListener("click", function () {
+        menuIcon.classList.toggle("active");
+        navLists.classList.toggle("active");
+    });
+
+    const dropdowns = document.querySelectorAll(".nav-item.dropdown");
+    dropdowns.forEach(dropdown => {
+        dropdown.addEventListener("click", function (e) {
+            e.stopPropagation();
+            const isActive = this.classList.contains("active");
+            closeAllDropdowns(); // Close all dropdowns before toggling the clicked one
+            if (!isActive) {
+                this.classList.add("active");
+            }
+        });
+    });
+
+    document.addEventListener("click", function () {
+        closeAllDropdowns();
+    });
+
+    function closeAllDropdowns() {
+        dropdowns.forEach(dropdown => {
+            dropdown.classList.remove("active");
+        });
+    }
+});
